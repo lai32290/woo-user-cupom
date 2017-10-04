15b9ef3134debcc60793b382442bba67e4cbb3a3
Lai <lai32690@gmail.com>
2017-10-03 22:30:36 -0300

    updateing page

--- templates/settings_page.php
+++ templates/settings_page.php
@@ -1,3 +1,24 @@
 <div>
-    <input type="text">
+    <h1>User's Cupom</h1>
+</div>
+<div>
+    <h3>Total: R$800,00</h3>
+</div>
+<div>
+    <table class="widefat striped">
+        <thead>
+        <tr>
+            <th>Item</th>
+            <th>Date</th>
+            <th>Value</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Item 1</td>
+            <td>02/02/2018</td>
+            <td>R$100,00</td>
+        </tr>
+        </tbody>
+    </table>
 </div>
